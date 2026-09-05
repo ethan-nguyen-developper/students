4bb35f4b8f25d6891fd9a9faf8b3763d90401f56
Ethan <mingam2007@gmail.com>
2026-09-05 17:44:06 +0200

    indexisation

--- app/Http/Controllers/NiveauScolaireController.php
+++ app/Http/Controllers/NiveauScolaireController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\NiveauScolaire;
 
 class NiveauScolaireController extends Controller
 {
     public function index(){
-        return inertia("NiveauScolaire/Index");
+        $niveauScolaires = NiveauScolaire::orderBy("nom", "ASC")->get();
+        // dd($niveauScolaires);
+        return inertia("NiveauScolaire/Index", [
+            "niveauScolaires" => $niveauScolaires
+        ]);
     }
 }
--- resources/js/Pages/NiveauScolaire/Index.vue
+++ resources/js/Pages/NiveauScolaire/Index.vue
@@ -13,19 +13,48 @@
 
         <div class="content">
             <div class="container-fluid">
-                <h1>mon contenu</h1>
+                <div class="card">
+                    <div class="card-header">
+                        <div class="card-tools">
+                        <ul class="pagination pagination-sm float-right">
+                            <li class="page-item"><a class="page-link" href="#">«</a></li>
+                            <li class="page-item"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item"><a class="page-link" href="#">»</a></li>
+                        </ul>
+                        </div>
+                    </div>
+                    <div class="card-body p-0">
+                        <table class="table">
+                        <thead>
+                            <tr>
+                            <th>Niveau scolaire</th>
+                            <th style="width: 100px">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="niveauScolaire in props.niveauScolaires">
+                            <td>{{niveauScolaire.nom}}</td>
+                            <td>
+                                <div class="d-flex justify-items-center">
+                                    <button class="btn btn-info me-2"><i class="fa-solid fa-pencil"></i></button>
+                                    <button class="btn btn-danger"><i class="fa-solid fa-trash"></i></button>
+                                </div>
+                            </td>
+                            </tr>
+                        </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     </main>
     <!--end::App Main-->
 </template>
 
-<script>
-export const app = "ma constante"
-
-export function tester() {
-
-}
-
-export default {}
+<script setup>
+const props = defineProps({ 
+    niveauScolaires: Array
+})
 </script>
